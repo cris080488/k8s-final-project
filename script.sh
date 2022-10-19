5e9d8b440d17ea74dce7ecf81354018d5114a1f0
crisosilva <crisosilva88@gmail.com>
2022-10-18 23:10:35 -0300

    corrected yml file

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
-echo "criando as imagens ......"
-docker build -t crisosilva88/k8s_project_backend:1.0 /backend/.
+#!/bin/bash
 
-docker build -t crisosilva88/k8s_project_database:1.0 /database/.
+echo "criando as imagens ......"
+docker build -t crisosilva88/k8s_project_backend:1.1 backend/.
+
+docker build -t crisosilva88/k8s_project_database:1.1 database/.
 
 echo "carregando as imagens ......"
-docker push crisosilva88/k8s_project_backend:1.0
+docker push crisosilva88/k8s_project_backend:1.1
 
-docker push crisosilva88/k8s_project_database:1.0
+docker push crisosilva88/k8s_project_database:1.1
 
 echo "Criando os Serviços ......"
 kubectl apply -f ./service.yml
